19a7ef1a50a006f7473502a31f9ea77e19ab3bc7
Robin <jacobrobin394@gmail.com>
2023-12-22 18:50:07 +0530

    Initial Commit

---
+++ src/main/java/com/example/BootStrap_A_Sample_Application/BootStrapASampleApplication.java
@@ -0,0 +1,17 @@
+package com.example.BootStrap_A_Sample_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.*"})
+@PropertySource(value = {"classpath:application.properties"} )
+public class BootStrapASampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStrapASampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BootStrap_A_Sample_Application/BootStrapASampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BootStrap_A_Sample_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStrapASampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
